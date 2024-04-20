ad3c9ecf6e4760687127db45b66baa40482db624
abhis12321 <abhisheksingh2359328@gmail.com>
2024-04-20 17:55:26 +0530

    a full stack ecommerse web app using my previous repo's backend data

---
+++ backend/controllers/authController.js
@@ -0,0 +1,20 @@
+const login = (req , res) => {
+    
+}
+const loginLoad = (req , res) => {
+    res.send({message:"hello jack!"})
+}
+const resister = (req , res) => {
+
+}
+const resisterLoad = (req , res) => {
+
+}
+
+
+module.exports = {
+    login,
+    loginLoad,
+    resister,
+    resisterLoad,
+}
