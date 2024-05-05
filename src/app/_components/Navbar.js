ed3959da23d2d05fa017daa0550ac7eb2151c4cc
abhis12321 <abhisheksingh2359328@gmail.com>
2024-05-06 04:13:58 +0530

    dark/ligh mode enabled for Navbar

--- src/app/_components/Navbar.js
+++ src/app/_components/Navbar.js
@@ -22,12 +22,12 @@ export default function NavBar() {
   };
 
   return (
-    <div className="w-full bg-gradient-to-b from-gray-400 to-slate-500 dark:from-slate-950 dark:to-slate-800 text-gray-300 font-semibold flex items-center justify-between h-16 px-4 xm:px-8 overflow-hidden">
+    <div className="w-full bg-gradient-to-b from-gray-500 to-lime-900 dark:from-slate-950 dark:to-lime-900 text-gray-300 font-semibold flex items-center justify-between h-16 px-4 xm:px-8 overflow-hidden">
       <div className="">
-        <Link href={'/'} className="font-bold text-3xl text-center rounded-md text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 w-fit leading-[4rem] font-mono"><span>E</span>commerse</Link>
+        <Link href={'/'} className="font-extrabold font-serif text-4xl text-center rounded-md text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 w-fit leading-[4rem] drop-shadow-[0_0_1px_black]"><span>E</span>commerse</Link>
       </div>
 
-      <div className="right-navbar fixed lg:static top-0 right-0 hidden lg:flex gap-8 bg-gradient-to-r lg:[background:none] from-gray-700 to-gray-600  dark:from-slate-800 dark:to-slate-950 min-h-[100vh] lg:min-h-fit z-10 items-center min-w-[170px] dark:font-normal font-medium">
+      <div className="right-navbar fixed lg:static top-0 right-0 hidden lg:flex gap-8 bg-gradient-to-l lg:[background:none] from-gray-500 to-lime-900 dark:from-gray-950 dark:to-lime-900 min-h-[100vh] lg:min-h-fit z-10 items-center min-w-[170px] dark:font-normal font-medium">
         <div
           className="text-4xl py-2 hover:text-red-800 text-center cursor-pointer"
           onClick={handleRight}
@@ -56,7 +56,7 @@ export default function NavBar() {
         <div className="w-full lg:w-fit flex items-center justify-center">
           <Link
             href="/login"
-            className="w-full text-3xl overflow-hidden flex items-center justify-center py-3 lg:py-3 lg:px-4 lg:rounded-lg hover:bg-slate-950/30 hover:text-white dark:hover:bg-gray-400/30 hover:lgshadow-[0_0_3px_gray]"
+            className="w-full text-3xl overflow-hidden flex items-center justify-center py-4 lg:py-3 lg:px-4 lg:rounded-lg hover:bg-slate-950/30 hover:text-white dark:hover:bg-gray-400/30 hover:lgshadow-[0_0_3px_gray]"
           >
           <FontAwesomeIcon icon={faUser} className="text-xl" />
           </Link>
@@ -67,7 +67,7 @@ export default function NavBar() {
             href="/cart"
             className="w-full text-2xl overflow-hidden flex items-center justify-center py-4 lg:py-3 lg:px-4 lg:rounded-lg hover:bg-slate-950/30 hover:text-white dark:hover:bg-gray-400/30 hover:lgshadow-[0_0_3px_gray]"
           >
-          <FontAwesomeIcon icon={faCartShopping} className="text-lg" />
+          <FontAwesomeIcon icon={faCartShopping} className="text-xl" />
           </Link>
         </div>
 
