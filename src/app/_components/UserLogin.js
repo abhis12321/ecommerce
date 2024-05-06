bb3fef84f0b43a2a766a9e7249e5af98f993e5eb
abhis12321 <abhisheksingh2359328@gmail.com>
2024-05-07 04:49:18 +0530

    registration page modified if login rediret to profile

--- src/app/_components/UserLogin.js
+++ src/app/_components/UserLogin.js
@@ -38,7 +38,7 @@ export default function UserLogin() {
         <div className="flex flex-col gap-5 items-center justify-center bg-no-repeat bg-center bg-contain " style={{ minHeight: "calc(100vh - 4rem)" }}>
             {
                 !USER.user ?
-                    <form className='text-white bg-gradient-to-t from-cyan-800 via-gray-500 to-cyan-950 dark:from-slate-900 dark:via-cyan-950 dark:to-slate-950 shadow-[0_0_7px_yellow] dark:shadow-[0_0_7px_white] w-[98%] max-w-[800px] py-8 rounded-xl min-h-[280px] opacity-90 flex flex-col gap-3 items-center' autoComplete='on' onSubmit={handleUserLogin}>
+                    <form className='text-white bg-gradient-radial from-rose-900 via-sky-800 to-cyan-900 dark:bg-gradient-to-tr  dark:from-slate-900 dark:via-cyan-950 dark:to-slate-950 shadow-[0_0_7px_yellow] dark:shadow-[0_0_7px_white] w-[98%] max-w-[800px] py-8 rounded-xl min-h-[280px] opacity-90 flex flex-col gap-3 items-center' autoComplete='on' onSubmit={handleUserLogin}>
                         <div className="flex flex-col sm:flex-row gap-2 items-center justify-around w-[98%] max-w-[600px] py-4">
                             <h1 className='mx-auto text-center py-1 text-3xl xs:text-4xl rounded-md w-fit font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-violet-700 drop-shadow-[0_0_1px_white]'>Login Page</h1>
                             <select name="status" value={role} onChange={(e) => setRole(e.target.value)} className=' outline-none invalid:text-pink-600 w-fit font-semibold shadow-[0_0_3px_white] bg-slate-950/70 hover:bg-slate-950 focus:bg-slate-950 py-2 px-3 rounded-md mx-auto text-center ring-cyan-600 ring-2' >
@@ -60,11 +60,11 @@ export default function UserLogin() {
                         </div>
                     </form>
                     :
-                    <div className='text-white bg-gradient-to-tr from-cyan-800 via-gray-500 to-cyan-950 dark:from-slate-900 dark:via-cyan-950 dark:to-slate-950 shadow-[0_0_7px_yellow] dark:shadow-[0_0_7px_white] w-[98%] max-w-[800px] min-h-[50vh] flex flex-col gap-3 items-center justify-evenly'>
-                        <h1 className=''>Hello Jack</h1>
-                        <p className="">{USER.user.name} <span className="text-gray-400">({USER.user.role})</span></p>
-                        <p className="">{USER.user.email}</p>
-                        <p className="">{USER.user.mobileNo}</p>
+                    <div className='text-white bg-gradient-radial from-rose-900 via-sky-700 to-cyan-900 dark:bg-gradient-to-tr dark:from-slate-900 dark:via-cyan-950 dark:to-slate-950 shadow-[0_0_7px_black] dark:shadow-[0_0_7px_white] w-[98%] max-w-[700px] min-h-[50vh] flex flex-col gap-3 items-center justify-evenly p-4 rounded-2xl'>
+                        <h1 className='text-yellow-400 drop-shadow-[0_0_3px_pink]'>You are Welcome, At Your Profile</h1>
+                        <p className=""><span className="text-gray-400">Name : </span>{USER.user.name} <span className="text-gray-400">({USER.user.role})</span></p>
+                        <p className=""><span className="text-gray-400">Email : </span>{USER.user.email}</p>
+                        <p className=""><span className="text-gray-400">MobileNo : </span>{USER.user.mobileNo}</p>
                         <button className='py-2 px-5 bg-red-900 hover:bg-red-800 hover:ring-2 ring-gray-400 rounded-lg' onClick={USER.logout}>Logout</button>
                     </div>
             }
--- src/app/registerUser/page.js
+++ src/app/registerUser/page.js
@@ -1,8 +1,18 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import UserRegister from '../_components/UserRegister'
+import { useAuth } from '../_components/AuthenticationProvider'
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+  const USER = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if(USER.user) {
+      router.push('/login')
+    }
+  })
+  
   return (
     <div className=''>
       <UserRegister  />
