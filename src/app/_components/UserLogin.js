884f9057d5b71d72da3db8b5de9a2c20cc65c26f
abhis12321 <abhisheksingh2359328@gmail.com>
2024-05-06 09:16:21 +0530

    Dark/Light mode enabled in UserLogin component

--- src/app/_components/UserLogin.js
+++ src/app/_components/UserLogin.js
@@ -17,9 +17,9 @@ export default function UserLogin({ role, setRole }) {
 
 
     return (
-        <div className="flex flex-col gap-5 items-center justify-center bg-slate-950 bg-no-repeat bg-center bg-contain " style={{ minHeight: "calc(100vh - 4rem)" }}>
-            <form className='bg-gradient-to-t from-slate-900 via-cyan-950 to-slate-950 shadow-[0_0_7px_white] w-[98%] max-w-[800px] py-8 rounded-xl min-h-[280px] opacity-90 flex flex-col gap-3' autoComplete='on' onSubmit={handleUserLogin}>
-                <h1 className='mx-auto py-1 text-center text-4xl rounded-md font-extrabold drop-shadow-[0_0_5px_black] text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 w-fit'>Login Page</h1>
+        <div className="flex flex-col gap-5 items-center justify-center bg-no-repeat bg-center bg-contain " style={{ minHeight: "calc(100vh - 4rem)" }}>
+            <form className='text-white bg-gradient-to-t from-cyan-900 dark:from-slate-900 via-gray-500 dark:via-cyan-950 to-cyan-900 dark:to-slate-950 shadow-[0_0_7px_white] w-[98%] max-w-[800px] py-8 rounded-xl min-h-[280px] opacity-90 flex flex-col gap-3' autoComplete='on' onSubmit={handleUserLogin}>
+                <h1 className='mx-auto py-1 text-center text-4xl rounded-md font-extrabold drop-shadow-[0_0_1px_black] text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-violet-600 w-fit'>Login Page</h1>
                 <h1 className="mb-2 text-center text-lg text-yellow-600 font-semibold drop-shadow-[0_0_5px_black]">Login with your email-id & Password</h1>
 
                 <select name="status" value={role} onChange={(e) => setRole(e.target.value)} className=' focus:ring-cyan-600 focus:ring-2 outline-none  bg-slate-950/70 hover:bg-slate-950 focus:bg-slate-950 block p-2 rounded mx-auto text-center w-[95%] max-w-[600px] shadow-[0_0_3px_white]' >
@@ -34,8 +34,8 @@ export default function UserLogin({ role, setRole }) {
                     <Link href={`/login/forgot`} className="drop-shadow-[0_0_5px_red]">forgot password</Link>
                 </div>
                 <div className="w-[95%] max-w-[600px] m-auto flex justify-between">
-                    <Link href={`/registerUser`} className='w-[130px] xs:w-[160px] text-center rounded-md bg-red-900 py-2 hover:bg-red-700 active:bg-indigo-900 font-mono' >Resister New</Link>
-                    <button type='submit' className='w-[130px] xs:w-[160px] text-center rounded-md bg-red-900 py-2 hover:bg-red-700 active:bg-indigo-900 font-mono' >Login</button>
+                    <Link href={`/registerUser`} className='w-[130px] xs:w-[160px] text-center rounded-md bg-red-900 py-2 hover:bg-red-700 active:bg-indigo-950 font-mono' >Resister New</Link>
+                    <button type='submit' className='w-[130px] xs:w-[160px] text-center rounded-md bg-red-900 py-2 hover:bg-red-700 active:bg-indigo-950 font-mono' >Login</button>
                 </div>
             </form>
         </div>
