2c0e4b2436e60f6baf284f5a2bad0d1161b75dd3
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-31 13:50:50 +0530

    User Model is modified for MongoDB storage tot store User Info

--- src/app/lib/models/User.js
+++ src/app/lib/models/User.js
@@ -3,47 +3,54 @@ import dbConnect from "../config/dbConnect";
 
 dbConnect();
 const userSchema = new mongoose.Schema({
-    name:{
+    userName:{
         type:String,
-        required:[true , 'Please enter user name'],
+        required:[true , 'data missing, Please enter your name'],
+    },
+    address: {
+        type:String,
+        required:[true , "data missing, Please enter your address"],        
+    },
+    countryCode:{
+        type:String,
+        default:"+91",
     },
     mobileNo:{
         type:String,
-        unique:true,
-        required:[true , "Please enter mobile number"],
+        unique:[true , "A user with the same mobile number is already registered."],
+        required:[true , "data missing, Please enter your mobile number"],
         minlength:[10 , "Mobile Number should contain atleast 10 digits"],
         maxlength:[10 , "Mobile Number should contain atmost 10 digits"],
     },
     email:{
         type:String,
-        unique:true,
-        required:[true , 'Please enter email Id'],
+        unique:[true , "A user with the same email-id is already registered."],
+        required:[true , 'data missing, Please enter your email Id'],
     },
     password:{
         type:String,
-        required:[true , 'Please enter password'],
+        required:[true , 'data missing, Please enter a password'],
     },
     image:{
-        public_id:{
-            type:String,
-            default:"NA"
-        },
-        url:{
-            type:String,
-            default:"/img/profile.png"
-        },
+        type:String,
+        default:'',
     },
     role:{
         type:String,
-        required:[true , 'Please enter user role'],
+        required:[true , 'data missing, Please select a user role'],
         enum: {
             values:[
                 "customer",
                 "seller",
+                "brand-admin",
                 "admin",
             ],
-            message:"Please select correct user role"
+            message:"Invalid value, Please select a correct user role"
         }
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
     },
 })
 
