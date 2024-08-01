b415b1a5348ef3b9ff68232d5e739c75c098bc62
abhis12321 <abhisheksingh2359328@gmail.com>
2024-08-01 21:38:02 +0530

    Rating Model is created along with its schema

---
+++ src/app/lib/models/RatingModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+const rating_schema = new mongoose.Schema({
+    product_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:[true , "product-id is missing."]
+    },
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:[true , "user-id is missing."]
+    },
+    rating:{
+        type:Number,
+        required:[true , "rating-starts are missing."]
+    },
+    review:{
+        type:String,
+    },
+    imgUrl:{
+        type:String,
+    },
+    rated_at:{
+        type:Date,
+        default:Date.now,
+    },
+});
+
+
+export const Rating = mongoose.models.Rating || mongoose.model("Rating" , rating_schema);
