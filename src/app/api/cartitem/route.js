fb34f43640823491e820c885792b2b992516a353
abhis12321 <abhisheksingh2359328@gmail.com>
2024-12-24 01:34:53 +0530

    POST api is created to add items to cart

--- src/app/api/cartitem/route.js
+++ src/app/api/cartitem/route.js
@@ -1,17 +1,28 @@
-import { NextResponse } from "next/server"
-import { getJWTUser } from "@/utils/getJWTUser"
-import { CartItem } from "@/lib/models/CartModel"
+import { NextResponse } from "next/server";
+import { getJWTUser } from "@/utils/getJWTUser";
+import { CartItem } from "@/lib/models/CartModel";
 
 
-export const GET = async() => {
+export const GET = async () => {
     try {
         const user = getJWTUser();
-        if(!user) {
-            return NextResponse.json({} , { status:404 })            
+        if (!user) {
+            return NextResponse.json({}, { status: 404 })
         }
         const cartItems = await CartItem.find({ user_id: user?._id });
         return NextResponse.json({ cartItems });
-    } catch(error) {
-        return NextResponse.json({} , { status:404 })
+    } catch (error) {
+        return NextResponse.json({}, { status: 404 });
+    }
+}
+
+export const POST = async (req) => {
+    try {
+        const { user_id, product_id, product_quantity } = await req.json();
+        const nvcartItem = new CartItem({ user_id, product_id, product_quantity });
+        await nvcartItem.save();
+        return NextResponse.json({ success: true, message: "product added to your cart" });
+    } catch (error) {
+        return NextResponse.json({}, { status: 404 });
     }
 }
